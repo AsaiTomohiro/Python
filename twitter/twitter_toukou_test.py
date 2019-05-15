2b802f299eb63d983ba7fa2848bca296f480b4ad
AsaiTomohiro <42804085+AsaiTomohiro@users.noreply.github.com>
2019-05-15 17:49:25 +0900

    Update twitter_toukou_test.py

--- twitter/twitter_toukou_test.py
+++ twitter/twitter_toukou_test.py
@@ -4,10 +4,10 @@
 import twitter
 
 # 取得したキーとアクセストークンを設定する
-auth = twitter.OAuth(consumer_key='fKZ3cldhN5CxZrmtQsSsdAsVY',
-                     consumer_secret='be8WrdAd9LvTfjpnDXL81o7GJtjMlS98hMRpWhJBmLSl3jRisD',
-                     token='1120628485158871040-PQd3lNIUxV8fyhNCcNI6RV9pYuHUfR',
-                     token_secret='x3jhRRgiWWlSfsaQb69NNHqytiDrqpxGEGK9GAzQ9qEBn')
+auth = twitter.OAuth(consumer_key='',
+                     consumer_secret='',
+                     token='',
+                     token_secret='')
 
 t = twitter.Twitter(auth=auth)
 
